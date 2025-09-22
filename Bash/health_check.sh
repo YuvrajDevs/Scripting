079499f7e08f015b57a9d2fc09fdbb662ef4dcc2
YuvrajDevs <workwithyuvraj0712@gmail.com>
2025-09-22 12:18:06 +0530

    modified the health_check script

--- Bash/health_check.sh
+++ Bash/health_check.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+DISK_THRESHOLD=80
+
 HOSTNAME=$(hostname)
-DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}')
+DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}'| sed 's/%//' )
 MEMORY_USAGE=$(free -m | awk 'NR==2 {print $3 "/" $2 " MB"}')
 
-echo "--- System Health Report ---"
-echo "Hostname: $HOSTNAME"
-echo "Disk Usage: $DISK_USAGE"
-echo "Memory Usage: $MEMORY_USAGE"
-echo "--------------------------"
+if [ $DISK_USAGE_PERCENT -gt $DISK_THRESHOLD ]; then
+  echo "WARNING: Disk usage is high: $DISK_USAGE_PERCENT%"
+else
+  echo "Disk usage is normal: $DISK_USAGE_PERCENT%"
+fi
